7b1f57ad57dffab466f637ff1c25694cb2580f0a
jfrancoc <jfcslc@outlook.com>
2019-06-20 13:42:49 -0600

    cities json

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,2 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "SUCCESS!";
-    var x = document.getElementsByTagName("BODY")[0];
-    x.style.backgroundColor = "red";
-}
+// 
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
